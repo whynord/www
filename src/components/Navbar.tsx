0e47332eda7301bcf15bda0668caabba12fd32e8
Nord <panpong@gmail.com>
2025-12-19 11:47:21 +0700

    - make logo 10px smaller

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -25,7 +25,7 @@ const Navbar = () => {
       <nav className="container mx-auto px-6 py-5 flex items-end justify-between">
         <a href="#home" className="flex gap-x-10">
           <div className="w-auto h-auto overflow-visible m-0">
-            <img src="/images/logo-2-2025green.png" width={110} alt="Nord profile" className="w-160" />
+            <img src="/images/logo-2-2025green.png" width={110} alt="Nord profile" />
           </div>
           <span className="hidden sm:block text-xs tracking-[0.15em] uppercase text-muted-foreground">
             Passion in Brand
